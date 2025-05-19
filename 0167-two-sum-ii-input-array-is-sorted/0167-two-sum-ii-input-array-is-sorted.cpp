703c7bbcab71a1b62dcc9c2a8327a3acbcaf4b3e
Pooja Mehta <161503607+pooja-169@users.noreply.github.com>
2025-05-19 10:56:48 +0530

    Time: 0 ms (100%), Space: 19.4 MB (63.37%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -16,3 +16,20 @@ public:
         return {};
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         int n=numbers.size();
+//         vector<int>v;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(numbers[i]+numbers[j]==target){
+//                     v.push_back(i+1);
+//                     v.push_back(j+1);
+//                 }
+//             }
+//         }
+//         return v;
+//     }
+// };
